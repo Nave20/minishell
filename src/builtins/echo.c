989b197ea9ccb31c9845dad172625a7ed565f67a
28 <vpirotti@z3r8p4.42lyon.fr>
2025-05-15 13:34:01 +0200

    adding builtin echo

---
+++ src/builtins/echo.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   echo.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vpirotti <vpirotti@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/13 12:41:00 by vpirotti          #+#    #+#             */
+/*   Updated: 2025/05/13 12:41:00 by vpirotti         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../header/minishell.h"
+#include "../../libft/libft.h"
+
+bool	valid_fag(const char *flag)
+{
+	int	i;
+
+	i = 1;
+	if (flag[0] != '-')
+		return (1);
+	while (flag[i])
+	{
+		if (flag[i] != 'n')
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+bool	ft_echo(char **content)
+{
+	bool	a;
+
+	a = 0;
+	if (!content)
+		return (ft_putendl_fd("", 1));
+	if (content[0])
+	{
+		if (valid_fag(content[0]) == 0)
+		{
+			if (ft_putstr_fd(content[1], 1) == -1)
+				return (1);
+			else
+				return (0);
+		}
+		else
+		{
+			if (ft_putstr_fd(content[0], 1) == -1)
+				a = 1;
+			if (write(1, " ", 1) == -1)
+				a = 1;
+			if (ft_putendl_fd(content[1], 1) == 1 || a == 1)
+				return (1);
+		}
+	}
+	return (0);
+}
